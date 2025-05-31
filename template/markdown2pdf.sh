cbcc8c55c5be3e293b9a8fe6dda94424b9d334f2
Martijn Pepping <111153+mpepping@users.noreply.github.com>
2025-05-31 12:25:16 +0200

    Render a simple PDF from a Markdown file

--- template/markdown2pdf.sh
+++ template/markdown2pdf.sh
@@ -2,4 +2,12 @@
 
 filename=${1}
 
-pandoc ${filename}.md -f markdown -o ${filename}.pdf --pdf-engine=lualatex -V geometry:a4paper
+pandoc ${filename}.md -f markdown -o ${filename}.pdf \
+--pdf-engine=lualatex -V geometry:a4paper \
+--template ./eisvogel.tex \
+--number-sections \
+--variable caption-justification=centering \
+-V mainfont="sourcesanspro" \
+-V sansfont="sourcesanspro" \
+-V monofont="sourcecodepro" \
+-V emoji-font="Noto Color Emoji"
